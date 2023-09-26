84066308b6394e77e7f776feb74434c0bc6a4574
Nikita Nedorezov <n.s.nedorezov@gmail.com>
2023-09-26 20:07:18 +0300

    add file

---
+++ TaskDB_2 SQL-запросы.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS genre (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS performer (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS album (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	release_year SMALLINT NOT NULL,
+	performer_id INTEGER REFERENCES performer(id)
+);
+
+CREATE TABLE IF NOT EXISTS track (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	duration VARCHAR(10) NOT NULL,
+	album_id INTEGER REFERENCES album(id)
+);
+
+CREATE TABLE IF NOT EXISTS collection (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	release_year SMALLINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS track_collection (
+	id SERIAL PRIMARY KEY,
+	track_id INTEGER REFERENCES track(id),
+	collection_id INTEGER REFERENCES collection(id)
+);
+
+CREATE TABLE IF NOT EXISTS genre_performer (
+	id SERIAL PRIMARY KEY,
+	genre_id INTEGER REFERENCES genre(id),
+	performer_id INTEGER REFERENCES performer(id)
+);
+
+CREATE TABLE IF NOT EXISTS album_performer (
+	id SERIAL PRIMARY KEY,
+	album_id INTEGER REFERENCES album(id),
+	performer_id INTEGER REFERENCES performer(id)
+);
